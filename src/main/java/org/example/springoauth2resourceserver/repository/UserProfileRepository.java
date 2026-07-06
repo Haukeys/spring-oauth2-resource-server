950d99a1474c2b6708bc31b4351a88d3ccb40475
Haukeys <leopazienza76@gmail.com>
2026-07-06 15:03:07 +0000

    FEAT: USERPROFILE REPOSITORY

---
+++ src/main/java/org/example/springoauth2resourceserver/repository/UserProfileRepository.java
@@ -0,0 +1,15 @@
+package org.example.springoauth2resourceserver.repository;
+
+import org.example.springoauth2resourceserver.entity.User_Profile;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public interface UserProfileRepository extends JpaRepository<User_Profile, UUID> {
+
+    // Permet de récupérer le profil directement à partir de l'UUID technique de l'User
+    Optional<User_Profile> findByUserUuid(UUID userUuid);
+}
