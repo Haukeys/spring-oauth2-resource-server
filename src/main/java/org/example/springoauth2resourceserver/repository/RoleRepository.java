44cec05c03b4c485a6e016e835382411ccced893
Haukeys <leopazienza76@gmail.com>
2026-07-06 15:03:17 +0000

    FEAT: ROLE REPOSITORY

---
+++ src/main/java/org/example/springoauth2resourceserver/repository/RoleRepository.java
@@ -0,0 +1,16 @@
+package org.example.springoauth2resourceserver.repository;
+
+import org.example.springoauth2resourceserver.entity.Role;
+import org.example.springoauth2resourceserver.entity.Roles;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, UUID> {
+
+    // Permet de récupérer l'entité Role à partir de son Enum (ADMIN, MANAGER, USER)
+    Optional<Role> findByRoles(Roles roles);
+}
