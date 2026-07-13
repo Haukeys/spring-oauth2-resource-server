fe3e2cedf2f58ad656cc9a470b8b7108a9750088
Haukeys <leopazienza76@gmail.com>
2026-07-13 15:14:41 +0000

    FEAT: ENABLE MONGO AUDITING IN SPRING OAUTH2 RESOURCE SERVER APPLICATION

--- src/main/java/org/example/springoauth2resourceserver/SpringOauth2ResourceServerApplication.java
+++ src/main/java/org/example/springoauth2resourceserver/SpringOauth2ResourceServerApplication.java
@@ -2,8 +2,10 @@ package org.example.springoauth2resourceserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.config.EnableMongoAuditing;
 
 @SpringBootApplication
+@EnableMongoAuditing
 public class SpringOauth2ResourceServerApplication {
 
     public static void main(String[] args) {
