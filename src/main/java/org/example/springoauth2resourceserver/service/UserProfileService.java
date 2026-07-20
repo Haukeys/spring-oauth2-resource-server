d6dede3cfbeef2f4a050f64734c7cfb23a5fc69f
Haukeys <leopazienza76@gmail.com>
2026-07-20 16:35:57 +0200

    FEAT: ADD METHOD TO RETRIEVE USER PROFILE WITH REDIS CACHE IN USERPROFILESERVICE INTERFACE

--- src/main/java/org/example/springoauth2resourceserver/service/UserProfileService.java
+++ src/main/java/org/example/springoauth2resourceserver/service/UserProfileService.java
@@ -1,7 +1,12 @@
 package org.example.springoauth2resourceserver.service;
 
+import org.example.springoauth2resourceserver.entity.User_Profile;
+
 import java.util.Map;
 
 public interface UserProfileService {
     Map<String, String> updateNickname(String sub, String nickname);
+
+    // La méthode pour récupérer le profil avec le cache Redis
+    User_Profile getUserProfileWithCache(String sub);
 }
