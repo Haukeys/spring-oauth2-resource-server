3fdb93687301dbfbc0bff7486b26b4096fae491b
Haukeys <leopazienza76@gmail.com>
2026-07-20 16:36:12 +0200

    FEAT: ADD CACHING FOR USER PROFILE RETRIEVAL AND NICKNAME UPDATE IN USERPROFILESERVICE

--- src/main/java/org/example/springoauth2resourceserver/service/UserProfileServiceImpl.java
+++ src/main/java/org/example/springoauth2resourceserver/service/UserProfileServiceImpl.java
@@ -3,6 +3,8 @@ package org.example.springoauth2resourceserver.service;
 import lombok.RequiredArgsConstructor;
 import org.example.springoauth2resourceserver.entity.User_Profile;
 import org.example.springoauth2resourceserver.repository.UserProfileRepository;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -17,8 +19,10 @@ public class UserProfileServiceImpl implements UserProfileService {
 
     @Override
     @Transactional
+    @CacheEvict(value = "userProfiles", key = "#sub")
     public Map<String, String> updateNickname(String sub, String nickname) {
         // 1. Vérifier si le pseudo est déjà pris dans MySQL
+
         if (userProfileRepository.existsByNickname(nickname)) {
             // On lève une exception que Spring pourra intercepter, ou une RuntimeException basique
             throw new IllegalArgumentException("The nickname '" + nickname + "' is already used.");
@@ -33,5 +37,13 @@ public class UserProfileServiceImpl implements UserProfileService {
         userProfileRepository.save(profile);
 
         return Collections.singletonMap("message", "Nickname '" + nickname + "' has been successfully registered!");
+    }
+
+    // Lecture depuis Redis (ou MySQL si absent du cache)
+    @Override
+    @Cacheable(value = "userProfiles", key = "#sub")
+    public User_Profile getUserProfileWithCache(String sub) {
+        return userProfileRepository.findByUserUuid_Sub(sub)
+                .orElseThrow(() -> new RuntimeException("USER PROFILE NOT FOUND"));
     }
 }
