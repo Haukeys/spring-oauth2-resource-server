b24d5dca259fce2dba2af15413ff1c9aac027a75
Haukeys <leopazienza76@gmail.com>
2026-07-23 12:45:49 +0200

    FEAT: ENABLE JPA AUDITING IN SPRING OAUTH2 RESOURCE SERVER APPLICATION

--- src/main/java/org/example/springoauth2resourceserver/SpringOauth2ResourceServerApplication.java
+++ src/main/java/org/example/springoauth2resourceserver/SpringOauth2ResourceServerApplication.java
@@ -2,10 +2,12 @@ package org.example.springoauth2resourceserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.mongodb.config.EnableMongoAuditing;
 
 @SpringBootApplication
 @EnableMongoAuditing
+@EnableJpaAuditing
 public class SpringOauth2ResourceServerApplication {
 
     public static void main(String[] args) {
