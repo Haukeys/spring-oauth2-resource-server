bc1116540b182626731104258288d74a3189af44
Haukeys <leopazienza76@gmail.com>
2026-07-27 16:46:48 +0200

    FEAT: ADD uploadAvatar method TO UserProfileService FOR AVATAR UPLOAD FUNCTIONALITY

--- src/main/java/org/example/springoauth2resourceserver/service/UserProfileService.java
+++ src/main/java/org/example/springoauth2resourceserver/service/UserProfileService.java
@@ -1,6 +1,8 @@
 package org.example.springoauth2resourceserver.service;
 
 import org.example.springoauth2resourceserver.entity.User_Profile;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.Map;
 
@@ -9,4 +11,6 @@ public interface UserProfileService {
 
     // La méthode pour récupérer le profil avec le cache Redis
     User_Profile getUserProfileWithCache(String sub);
+
+    String uploadAvatar(Jwt jwt, MultipartFile file) throws Exception;
 }
